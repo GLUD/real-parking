194bd5607b4c121c7246aec18f6d20d751f0735b
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-11-12 15:01:43 -0500

    se documenta

--- hardware/transceiver-tx-mode/src/transceiver-tx-mode.ino
+++ hardware/transceiver-tx-mode/src/transceiver-tx-mode.ino
@@ -12,7 +12,7 @@
 
 RF24 radio(9,10);
 
-const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL }; //LongLong = 64 bits.
 
 void setup(void)
 {
